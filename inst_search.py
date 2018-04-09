9aa97be566ddb4695ed423fe83057d44282cec75
jvshk78 <jvshk78trade@gmail.com>
2018-04-09 12:05:28 +0530

    v1.0

---
+++ inst_search.py
@@ -0,0 +1,15 @@
+while (i<=20000):
+    if(inst[i]['tradingsymbol']=='CRUDEOILM18APRFUT'):
+        print(inst[i]['instrument_token'])
+    i=i+1
+i=0
+
+while (i <= 20000):
+        if (inst[i]['instrument_token'] == 53556999):
+            print(inst[i]['tradingsymbol'])
+        i = i + 1
+i=0
+
+CRUDEOIL18APRFUT = [53556743]
+CRUDEOILM18APRFUT=[53556999]
+SBIN = [128028676]
